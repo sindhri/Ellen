64f328ac637ee809ceb409a68d5038e712537ab8
Jia Wu <jiawu.jw@gmail.com>
2020-08-14 16:09:26 -0400

    Delete practice_R.R

--- practice_R.R
+++
@@ -1,6 +0,0 @@
-library(readr)
-library(dplyr)
-data <- read_csv("individualData.csv")
-data_summary <- data %>%
-  group_by(genotype,plate) %>%
-  summarize(mean = mean(averageActivity_day1), sd = sd(averageActivity_day1))
