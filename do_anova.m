22a7128daa844513aa444b0f14f9363f5abc5d38
Jia Wu <jiawu.jw@gmail.com>
2020-10-23 11:55:04 -0400

    change anova model to include individual fish

--- do_anova.m
+++ do_anova.m
@@ -1,17 +1,20 @@
-%todo
-%2. make the anova more friendly
+%20201022, modify the model to use individual fish
+%only run the 3-way interaction
+%add a post hoc t test
+%friendly output of all the results
 
 %20201013, analysis on HOM alone and ST alone, compare pre and post
 %20200929, saved tables
 %try use the difference wave of pre and post, then 3-way anova
 %only use the mean across the fish in that geno across time
 
-function combined_table = do_anova(output)
+function results = do_anova(output)
 
-    pre_table = output.pre_geno_mean;
-    post_table = output.post_geno_mean;
-    geno_table_index = 1;
-    activity_col_index = 3:size(pre_table,2); %skip the first two columns, geno, GroupCount
+    pre_table = output.pre_fish_mean;
+    post_table = output.post_fish_mean;
+    geno_table_index = 2;
+    col_to_skip = 3;
+    activity_col_index = col_to_skip+1:size(pre_table,2); %skip the first two columns, geno, GroupCount
     
     % combine the pre and post table, add time(either pre_ or post), 
     % and extract the geno column into three factors
@@ -30,21 +33,25 @@ function combined_table = do_anova(output)
     if exist(destination_folder,'dir')~=7
        mkdir(destination_folder);
     end     
-    anova_plot(diff_table, 'diff', activity_col_index, destination_folder);
-    anova_plot(HOM_table, 'HOM', activity_col_index, destination_folder);
-    anova_plot(WT_table, 'WT', activity_col_index, destination_folder);
+    results = struct;
+    results = anova_plot(diff_table, 'diff', activity_col_index, ...
+        destination_folder,results);
+    results = anova_plot(HOM_table, 'HOM', activity_col_index, ...
+        destination_folder,results);
+    results = anova_plot(WT_table, 'WT', activity_col_index, ...
+        destination_folder,results);
 end
 
 % run three types, diff, HOM, WT
-function anova_plot(data_table, type, activity_col_index, destination_folder)
+function results = anova_plot(data_table, type, activity_col_index, destination_folder,results)
     if strcmp(type, 'diff') == 0 && strcmp(type, 'HOM') == 0 && strcmp(type, 'WT') == 0
         fprintf('type has to be either diff, HOM, or WT, abort\n')
         return
     end
-    
     for i = 1:length(activity_col_index)
             activity_name = data_table.Properties.VariableNames{activity_col_index(i)};
             activity_data = data_table{:,activity_col_index(i)};
+            anova_name = [type '_' activity_name];
         % 3-way anova with interaction
         % for diff_table
             %factor1: geno-- HOM vs. WT
@@ -57,17 +64,25 @@ function anova_plot(data_table, type, activity_col_index, destination_folder)
         if strcmp(type, 'diff')==1
             [p,tbl,stats] = anovan(activity_data,{data_table.factor1,...
                 data_table.factor2, data_table.factor3},...
-                'model','interaction','varnames',{'Geno','drug1','drug2'});
+                'model',[1,1,1],'varnames',{'Geno','drug1','drug2'});
+            results.data{i,1} = [tbl{2,3}, tbl{3,3}, tbl{2,6},tbl{2,7}];
         else
             [p,tbl,stats] = anovan(activity_data,{data_table.time,...
                 data_table.factor2,data_table.factor3},...
-                'model','interaction','varnames',{'Time','drug1','drug2'});
+                'model',[1,1,1],'varnames',{'Time','drug1','drug2'});
+            if strcmp(type,'HOM')==1
+               results.data{i,2} = [tbl{2,3}, tbl{3,3}, tbl{2,6},tbl{2,7}];
+            else
+               results.data{i,3} = [tbl{2,3}, tbl{3,3}, tbl{2,6},tbl{2,7}];
+            end
+
         end
+        
+        results.names{i} = anova_name;
         figure1 = figure(1);
         set(figure1, 'PaperPosition', [0 0 8 4],'PaperPositionMode','auto');
         set(figure1,'PaperSize',[8,4]);
-        saveas(figure1,[destination_folder '/anova_' type '_' activity_name],'pdf');
+        saveas(figure1,[destination_folder '/anova_' anova_name],'pdf');
         close(figure1);
-        
     end
 end
